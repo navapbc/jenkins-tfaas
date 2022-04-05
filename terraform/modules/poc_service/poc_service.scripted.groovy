db68959da33ac83aa06a59dadcc6a90ab0434e43
Jimmy Carter <jimmycarter@navapbc.com>
2022-04-05 12:47:07 -0700

    added buildName setter

--- terraform/modules/poc_service/poc_service.scripted.groovy
+++ terraform/modules/poc_service/poc_service.scripted.groovy
@@ -1,10 +1,10 @@
 node {
     stage('Preparation') { // for display purposes
         sh 'date'
+        currentBuild.displayName = "poc v1.2.3"
     }
     stage('Build') {
         sh 'date'
-
     }
     stage('Results') {
         sh 'date'
