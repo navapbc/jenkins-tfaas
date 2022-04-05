154f66ec2e2b324154980d101b43f24fd50df14c
Jimmy Carter <jimmycarter@navapbc.com>
2022-04-05 12:49:06 -0700

    bigger name

--- terraform/modules/poc_service/poc_service.scripted.groovy
+++ terraform/modules/poc_service/poc_service.scripted.groovy
@@ -1,7 +1,7 @@
 node {
     stage('Preparation') { // for display purposes
         sh 'date'
-        currentBuild.displayName = "poc v1.2.3"
+        currentBuild.displayName = "poc v1.2.3 TF: 0/3/0"
     }
     stage('Build') {
         sh 'date'
