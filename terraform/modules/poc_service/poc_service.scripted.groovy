5a9da8efd7e65df89e02d7c97454beef97bba85b
Jimmy Carter <jimmycarter@navapbc.com>
2022-04-05 13:29:24 -0700

    TF stage-view labels

--- terraform/modules/poc_service/poc_service.scripted.groovy
+++ terraform/modules/poc_service/poc_service.scripted.groovy
@@ -1,12 +1,15 @@
 node {
     stage('Preparation') { // for display purposes
         sh 'date'
-        currentBuild.displayName = "poc v1.2.3 TF: 0/3/0"
+        currentBuild.displayName = "[poc_service-v1.2.3] TF: 0/3/0"
     }
-    stage('Build') {
+    stage('TF Plan') {
         sh 'date'
     }
-    stage('Results') {
+    stage('TF Apply') {
+        sh 'date'
+    }
+    stage('TF Post-Plan') {        
         sh 'date'
     }
 }
